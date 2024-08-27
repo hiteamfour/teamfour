8fb11df99e65c539377330adb066481bc155a778
junsik <ofice89@naver.com>
2024-08-27 12:34:46 +0900

    DAO, DTO 잠깐 수정했어요 빈 파일처럼 보일 수 있어요

--- teamfour/src/main/java/com/ohgiraffers/teamfour/employee/model/dto/EmployeeDTO.java
+++ teamfour/src/main/java/com/ohgiraffers/teamfour/employee/model/dto/EmployeeDTO.java
@@ -170,5 +170,6 @@ public class EmployeeDTO implements java.io.Serializable {
                 ", entDate=" + entDate +
                 ", entYn='" + entYn + '\'' +
                 '}';
+
     }
 }
