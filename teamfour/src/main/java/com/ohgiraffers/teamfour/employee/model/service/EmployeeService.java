2d3d408f34bce13f03f9243629accc49aef65976
kalien12 <kalien.lee.1102@gmail.com>
2024-08-27 15:06:39 +0900

    커밋 준비 완료

--- teamfour/src/main/java/com/ohgiraffers/teamfour/employee/model/service/EmployeeService.java
+++ teamfour/src/main/java/com/ohgiraffers/teamfour/employee/model/service/EmployeeService.java
@@ -5,8 +5,7 @@ import com.ohgiraffers.teamfour.employee.model.dto.EmployeeDTO;
 
 import java.sql.Connection;
 
-import static com.ohgiraffers.teamfour.employee.common.jdbc.JDBCTemplate.close;
-import static com.ohgiraffers.teamfour.employee.common.jdbc.JDBCTemplate.getConnection;
+import static com.ohgiraffers.teamfour.employee.common.jdbc.JDBCTemplate.*;
 
 public class EmployeeService {
 
@@ -28,5 +27,40 @@ public class EmployeeService {
         /* 조회 결과를 반환한다. */
         return selectedEmp;
     }
+
+    public String selectNewEmpId() {
+
+        /* Connection 생성 */
+        Connection con = getConnection();
+
+        /* 비지니스 로직 */
+        /* 1. dao 호출하여 조회 */
+        String newEmpId = empDAO.selectNewEmpId(con);
+
+        /* Connection 닫기 */
+        close(con);
+
+        /* 수행 결과 반환 */
+        return newEmpId;
+    }
+
+    public int insertEmp(EmployeeDTO emp) {
+
+        Connection con = getConnection();
+
+        int result = empDAO.insertEmp(con, emp);
+
+        if (result > 0) {
+            commit(con);
+        } else {
+            rollback(con);
+        }
+
+        close(con);
+
+        return result;
+    }
+
+
 
 }
