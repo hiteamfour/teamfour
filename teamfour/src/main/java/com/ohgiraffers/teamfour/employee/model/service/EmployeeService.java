fb915de52231a1e792a9c47d4a99b10fef4d6cda
kalien12 <kalien.lee.1102@gmail.com>
2024-08-27 14:45:56 +0900

    상기의 내용 확인 및 업로드 준비 완료

--- teamfour/src/main/java/com/ohgiraffers/teamfour/employee/model/service/EmployeeService.java
+++ teamfour/src/main/java/com/ohgiraffers/teamfour/employee/model/service/EmployeeService.java
@@ -1,4 +1,51 @@
 package com.ohgiraffers.teamfour.employee.model.service;
 
+import com.ohgiraffers.teamfour.employee.model.dao.EmployeeDAO;
+import com.ohgiraffers.teamfour.employee.model.dto.EmployeeDTO;
+
+import java.sql.Connection;
+
+import static com.ohgiraffers.teamfour.employee.common.jdbc.JDBCTemplate.*;
+
 public class EmployeeService {
+
+    private final EmployeeDAO empDAO;
+
+    public EmployeeService() {
+        empDAO = new EmployeeDAO();
+    }
+
+
+    public String selectNewEmpId() {
+
+        /* Connection 생성 */
+        Connection con = getConnection();
+
+        /* 비지니스 로직 */
+        /* 1. dao 호출하여 조회 */
+        String newEmpId = empDAO.selectNewEmpId(con);
+
+        /* Connection 닫기 */
+        close(con);
+
+        /* 수행 결과 반환 */
+        return newEmpId;
+    }
+
+    public int insertEmp(EmployeeDTO emp) {
+
+        Connection con = getConnection();
+
+        int result = empDAO.insertEmp(con, emp);
+
+        if (result > 0) {
+            commit(con);
+        } else {
+            rollback(con);
+        }
+
+        close(con);
+
+        return result;
+    }
 }
